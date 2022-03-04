d1231f32251244d1917437b01e08a9dbdf079f09
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-03-04 18:30:13 +0100

    User Logout

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,11 +1,7 @@
 import { useState } from 'react'
 import { toast } from 'react-toastify'
 import { Link, useNavigate } from 'react-router-dom'
-import {
-	getAuth,
-	sigInWithEmailAndPassword,
-	signInWithEmailAndPassword,
-} from 'firebase/auth'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
